0486b356816d86b2cf37b1cd3f191dadcb26fd10
jakesummer <jakersummer@gmail.com>
2026-03-22 11:05:52 -0400

    Add operator function to call other 4 functions

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+    answer = 
+        (operator === "+") ? add(num1, num2):
+        (operator === "-") ? subtract(num1, num2):
+        (operator === "x") ? multiply(num1, num2):
+        (operator === "÷") ? divide(num1, num2):
+        "";
+    return answer;
+}
