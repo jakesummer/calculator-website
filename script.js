8e86ddce8b7fe643b7c96062c6088fac8f4a505f
jakesummer <jakersummer@gmail.com>
2026-03-22 11:52:18 -0400

    Handle button clicks and allow operations to occur

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let calculator = {
+    firstNum: null,
+    operator: null,
+    currentInput: "",
+};
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -23,3 +29,35 @@ function operate(num1, operator, num2) {
         "";
     return answer;
 }
+
+function handleOperator(operator) {
+    if (operator === "=") {
+        if (calculator.firstNum && calculator.operator && calculator.currentInput) {
+            operate(calculator.firstNum, calculator.operator, calculator.currentInput);
+            calculator.firstNum = null;
+            calculator.operator = null;
+            calculator.currentInput = "";
+        }
+    } else {
+        if (calculator.currentInput && !calculator.operator && !calculator.firstNum){
+            calculator.firstNum = calculator.currentInput;
+            calculator.operator = operator;
+            calculator.currentInput = "";
+        }
+    }
+}
+
+const digitButtons = document.querySelector(".digit-buttons");
+const operatorButtons = document.querySelector(".operator-buttons");
+
+digitButtons.addEventListener("click", (e) => {
+    if (e.target.nodeName !== "BUTTON") return;
+    let digit = e.target.textContent;
+    calculator.currentInput += digit;
+})
+
+operatorButtons.addEventListener("click", (e) => {
+    if (e.target.nodeName !== "BUTTON") return;
+    let operator = e.target.textContent;
+    handleOperator(operator);
+})
